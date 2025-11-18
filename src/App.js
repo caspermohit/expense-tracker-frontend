538fcf025ebaf605fa8d0a21fe6d61b506659c88
mohit shah <41099035+caspermohit@users.noreply.github.com>
2025-11-17 22:37:28 -0500

    full frontend update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState , useEffect } from 'react';
+import axios from 'axios';
+
+
+
 
 function App() {
+  
+  const [message, setMessage] = useState("");
+ useEffect(() => {
+  axios.get(`${process.env.REACT_APP_API_URL}/expenses`)
+  .then((res) =>setMessage(res.data.message))
+  .catch((err) => console.log(err));
+ },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={{textAlign:'center' , marginTop:'50px'}}>
+    <h1>Expense Tracker</h1>
+   <h2> {message} </h2>
+
+   </div>
   );
 }
 
